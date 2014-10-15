05f5e5c7640a3628ef3770d4915fc762d51f36dd
Kevin Dias <diasks2@gmail.com>
2014-10-16 07:33:21 +0900

    Add jieba code

--- server.rb
+++ server.rb
@@ -1,16 +1,9 @@
-# -*- encoding : utf-8 -*-
-#
-
-require 'jvn_segmenter'
+require 'jieba'
 require 'json'
-
-def segmenter
-  @@segmenter ||= JvnSegmenter.new
-end
 
 post "/?" do
   return 403 unless params[:key] == ENV['KEY']
   text = params[:text]
-  segmented_text = segmenter.segment text
+  segmented_text = text.to_tags
   segmented_text.scan(/\[([^\]]*)\]/).flatten.to_json
 end
