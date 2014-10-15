4e7fcbd2fc72d82341354f6272763c21978bfff7
Kevin Dias <diasks2@gmail.com>
2014-10-16 08:05:35 +0900

    Work on encoding

--- server.rb
+++ server.rb
@@ -7,5 +7,5 @@ post "/?" do
   return 403 unless params[:key] == ENV['KEY']
   text = params[:text]
   segmented_text = text.to_tags.to_s
-  segmented_text.scan(/\[([^\]]*)\]/).flatten.to_json
+  segmented_text.force_encoding("utf-8")#.scan(/\[([^\]]*)\]/).flatten.to_json
 end
