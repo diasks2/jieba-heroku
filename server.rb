8934aa6bc714dd7ae44f5cca7ddc717a60a307a7
Kevin Dias <diasks2@gmail.com>
2014-10-16 08:35:06 +0900

    Work on encoding issues

--- server.rb
+++ server.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*- #
+# -*- encoding : utf-8 -*-
 
 require 'jieba'
 require 'json'
@@ -7,5 +7,5 @@ post "/?" do
   return 403 unless params[:key] == ENV['KEY']
   text = params[:text]
   segmented_text = text.to_tags.to_s
-  segmented_text.force_encoding("utf-8")#.scan(/\[([^\]]*)\]/).flatten.to_json
+  segmented_text #.scan(/\[([^\]]*)\]/).flatten.to_json
 end
--- start_up.rb
+++ start_up.rb
@@ -1,4 +1,5 @@
 # -*- encoding : utf-8 -*-
+
 # required gem files
 require 'sinatra'
 
